3d8562fbcb45de537a3451725407f885d8aae050
IsaRobinson <81049637+IsaRobinson@users.noreply.github.com>
2022-05-17 22:26:32 -0400

    17.05.22

---
+++ EscalArt/static/escalArt/js/publicacion.js
@@ -0,0 +1,14 @@
+// Opciones perfil artista
+var toggle = false;
+
+function MostrarOpciones() {
+    if (toggle) {
+        document.getElementById('settings-post').style.display = 'none';
+        toggle = false;
+
+    } else {
+        document.getElementById('settings-post').style.display = 'flex';
+        toggle = true;
+
+    }
+}
