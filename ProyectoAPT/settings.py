16b286d0cc8161c619c71fe26c1df4a4520c4567
IsaRobinson <81049637+IsaRobinson@users.noreply.github.com>
2022-05-22 01:38:48 -0400

    22.05.22

--- ProyectoAPT/settings.py
+++ ProyectoAPT/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'EscalArt',
-    'crispy_forms'
+    'crispy_forms',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT= 'static'
+
+
+TAGGIT_CASE_INSENSITIVE = True
