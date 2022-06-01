5a3c0d3d0ec3abfa4f5caf30e6a49d8616557f76
IsaRobinson <81049637+IsaRobinson@users.noreply.github.com>
2022-05-31 22:19:56 -0400

    31.05.22

--- EscalArt/static/escalArt/js/room.js
+++ EscalArt/static/escalArt/js/room.js
@@ -2,7 +2,7 @@ const chatLog = document.querySelector('#chat-log')
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
 
 
-if (chatLog.hasChildNodes() <= 1) {
+if (chatLog.childNodes.length <= 1) {
     const emptyText = document.createElement('h3')
     emptyText.id = 'emptyText'
     emptyText.innerText = 'Este es el inicio de su conversacion, di algo!'
@@ -21,7 +21,15 @@ const chatSocket = new WebSocket(
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
     const divMensaje = document.createElement('div')
-    divMensaje.className = 'mensaje'
+    const userId = data['user_idUser']
+    const loggedInUserId = JSON.parse(document.getElementById('user_idUser').textContent)
+
+    if (userId === loggedInUserId) {
+        divMensaje.classList.add('mensaje', 'left')
+    } else {
+        divMensaje.className = 'mensaje'
+    }
+
     const messageElement = document.createElement('div')
         // messageElement.innerText = data.message
     messageElement.className = 'cuerpo'
@@ -44,6 +52,7 @@ chatSocket.onclose = function(e) {
 
 document.querySelector('#chat-message-input').focus();
 document.querySelector('#chat-message-input').onkeyup = function(e) {
+
     if (e.keyCode === 13) { // enter, return
         document.querySelector('#chat-message-submit').click();
     }
@@ -52,8 +61,10 @@ document.querySelector('#chat-message-input').onkeyup = function(e) {
 document.querySelector('#chat-message-submit').onclick = function(e) {
     const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
-    chatSocket.send(JSON.stringify({
-        'message': message
-    }));
-    messageInputDom.value = '';
+    if (message.length >= 1) {
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        messageInputDom.value = '';
+    }
 };
