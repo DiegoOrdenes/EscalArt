45fcfcb2ba04a6986df7a730989deee57599fe37
IsaRobinson <81049637+IsaRobinson@users.noreply.github.com>
2022-06-04 00:32:03 -0400

    04.06.22

---
+++ EscalArt/static/escalArt/js/configuracion.js
@@ -0,0 +1,93 @@
+/* Usuario/Cliente */
+function Editarperfil() {
+    document.getElementById('Editar-perfil').style.display = 'block';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
+
+function Cambiarpass() {
+    document.getElementById('Cambiar-pass').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
+
+function seguridad() {
+    document.getElementById('seguridad').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
+
+function privacidad() {
+    document.getElementById('privacidad').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
+
+function Notificaciones() {
+    document.getElementById('Notificaciones').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
+
+function accesibilidad() {
+    document.getElementById('accesibilidad').style.display = 'block';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+}
+
+
+
+/* Artista */
+function Editar_perfil_Artista() {
+    document.getElementById('Editar-perfil-Artista').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Cambiar-pass-Artista').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+    document.getElementById('Categoria-Artista').style.display = 'none';
+}
+
+function Cambiar_pass_Artista() {
+    document.getElementById('Cambiar-pass-Artista').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Editar-perfil-Artista').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+    document.getElementById('Categoria-Artista').style.display = 'none';
+}
+
+function Categoria_Artista() {
+    document.getElementById('Categoria-Artista').style.display = 'block';
+    document.getElementById('Editar-perfil').style.display = 'none';
+    document.getElementById('Editar-perfil-Artista').style.display = 'none';
+    document.getElementById('Cambiar-pass').style.display = 'none';
+    document.getElementById('Cambiar-pass-Artista').style.display = 'none';
+    document.getElementById('seguridad').style.display = 'none';
+    document.getElementById('privacidad').style.display = 'none';
+    document.getElementById('Notificaciones').style.display = 'none';
+    document.getElementById('accesibilidad').style.display = 'none';
+}
